93e1a64068eafe3c65e0446aa1113854a05162db
mike.zhang <mike.zhang@moneycorp.com>
2018-02-19 16:45:13 +0000

    intial Setup

---
+++ AnyaSpa.ClientTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AnyaSpa.ClientTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
