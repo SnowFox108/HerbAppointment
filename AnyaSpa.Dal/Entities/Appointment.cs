017bc8244b17fd7e8d4f1d91b45297cea5f1b03b
mike.zhang <mike.zhang@moneycorp.com>
2018-02-22 17:25:31 +0000

    update changes

---
+++ AnyaSpa.Dal/Entities/Appointment.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AnyaSpa.Infrastructure.Entities;
+
+namespace AnyaSpa.Dal.Entities
+{
+    public class Appointment : IEntity
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ AnyaSpa.Dal/Enums/AppointmentStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace AnyaSpa.Dal.Enums
+{
+    public enum AppointmentStatus
+    {
+        Book = 1,
+        Confirmed = 2,
+        Checkout = 3,
+        Cancel = 4,
+    }
+}
